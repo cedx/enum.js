2bdb7f6f895e51617fe69290c91537a4b135a002
Cédric Belin <cedric@belin.io>
2017-08-25 13:20:33 +0200

    Added the `getEntries()` method

--- test/enum_test.js
+++ test/enum_test.js
@@ -38,6 +38,8 @@ describe('Enum', () => {
 
     it('should create types having the `Enum` mixins', () => {
       expect(SampleEnum.isDefined).to.be.a('function');
+      expect(SampleEnum.getEntries).to.be.a('function');
+      expect(SampleEnum.getIndex).to.be.a('function');
       expect(SampleEnum.getName).to.be.a('function');
       expect(SampleEnum.getNames).to.be.a('function');
       expect(SampleEnum.getValues).to.be.a('function');
@@ -49,6 +51,7 @@ describe('Enum', () => {
    */
   describe('.isDefined()', () => {
     it('should return `false` for unknown values', () => {
+      expect(SampleEnum.isDefined(0)).to.be.false;
       expect(SampleEnum.isDefined('TWO')).to.be.false;
       expect(SampleEnum.isDefined(3.1)).to.be.false;
     });
@@ -62,10 +65,56 @@ describe('Enum', () => {
   });
 
   /**
+   * @test {Enum.getEntries}
+   */
+  describe('.getEntries()', () => {
+    it('should return the pairs of names and values of the enumerated constants', () => {
+      let entries = SampleEnum.getEntries();
+      expect(entries).to.have.lengthOf(4);
+      for (let entry of entries) expect(entry).to.be.an('array').and.have.lengthOf(2);
+
+      let [name, value] = entries[0];
+      expect(name).to.equal('ZERO');
+      expect(value).to.be.false;
+
+      [name, value] = entries[1];
+      expect(name).to.equal('ONE');
+      expect(value).to.equal(1);
+
+      [name, value] = entries[2];
+      expect(name).to.equal('TWO');
+      expect(value).to.equal('two');
+
+      [name, value] = entries[3];
+      expect(name).to.equal('THREE');
+      expect(value).to.equal(3.0);
+    });
+  });
+
+  /**
+   * @test {Enum.getIndex}
+   */
+  describe('.getIndex()', () => {
+    it('should return `-1` for unknown values', () => {
+      expect(SampleEnum.getIndex(0)).to.equal(-1);
+      expect(SampleEnum.getIndex('TWO')).to.equal(-1);
+      expect(SampleEnum.getIndex(3.1)).to.equal(-1);
+    });
+
+    it('should return the index of the enumerated constant for known values', () => {
+      expect(SampleEnum.getIndex(false)).to.equal(0);
+      expect(SampleEnum.getIndex(1)).to.equal(1);
+      expect(SampleEnum.getIndex('two')).to.equal(2);
+      expect(SampleEnum.getIndex(3.0)).to.equal(3);
+    });
+  });
+
+  /**
    * @test {Enum.getName}
    */
   describe('.getName()', () => {
     it('should return an empty string for unknown values', () => {
+      expect(SampleEnum.getName(0)).to.be.empty;
       expect(SampleEnum.getName('TWO')).to.be.empty;
       expect(SampleEnum.getName(3.1)).to.be.empty;
     });
