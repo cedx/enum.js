2218726dd6c68cc7fbacdfa9d0b8232d96a1440a
Cédric Belin <cedric@belin.io>
2018-08-12 22:54:59 +0200

    Updated the build system

--- karma.conf.js
+++ karma.conf.js
@@ -1,8 +1,14 @@
 module.exports = config => config.set({
   browsers: ['Chrome'],
-  browserify: {debug: true},
-  client: {mocha: {opts: true}},
-  frameworks: ['browserify', 'mocha'],
-  files: ['test/**/*.js'],
-  preprocessors: {'test/**/*.js': ['browserify']}
+  files: ['src/**/*.ts', 'test/**/*.ts'],
+  frameworks: ['mocha', 'karma-typescript'],
+  karmaTypescriptConfig: {
+    include: ['test/**/*.ts'],
+    tsconfig: 'tsconfig.json'
+  },
+  preprocessors: {
+    'src/**/*.ts': ['karma-typescript', 'coverage'],
+    'test/**/*.ts': ['karma-typescript']
+  },
+  reporters: ['progress', 'karma-typescript']
 });
