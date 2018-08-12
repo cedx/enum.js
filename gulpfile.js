d774208d8fe85c103999d1b763ef749c8019a8af
Cédric Belin <cedric@belin.io>
2018-08-12 22:54:59 +0200

    Updated the build system

--- gulpfile.js
+++ gulpfile.js
@@ -29,7 +29,7 @@ gulp.task('coverage', () => _exec('node_modules/.bin/coveralls', ['var/lcov.info
 /**
  * Builds the documentation.
  */
-gulp.task('doc:api', () => _exec('node_modules/.bin/esdoc'));
+gulp.task('doc:api', () => _exec('node_modules/.bin/typedoc'));
 gulp.task('doc:web', () => _exec('mkdocs', ['build']));
 gulp.task('doc', gulp.series('doc:api', 'doc:web'));
 
@@ -47,7 +47,7 @@ gulp.task('lint', () => _exec('node_modules/.bin/tslint', sources));
  * Runs the unit tests.
  */
 gulp.task('test:browser', () => _exec('node_modules/.bin/karma', ['start', '--single-run']));
-gulp.task('test:node', () => _exec('node_modules/.bin/nyc', [normalize('node_modules/.bin/mocha'), 'test/**/*.ts']));
+gulp.task('test:node', () => _exec('node_modules/.bin/nyc', [normalize('node_modules/.bin/mocha')]));
 gulp.task('test', gulp.series('test:browser', 'test:node'));
 
 /**
@@ -57,7 +57,7 @@ gulp.task('upgrade', async () => {
   await _exec('git', ['reset', '--hard']);
   await _exec('git', ['fetch', '--all', '--prune']);
   await _exec('git', ['pull', '--rebase']);
-  await _exec('npm', ['install']);
+  await _exec('npm', ['install', '--ignore-scripts']);
   return _exec('npm', ['update']);
 });
 
