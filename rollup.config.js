67be79959608a50beef2acf3656767882db619bb
Cédric Belin <cedric@belin.io>
2019-04-14 14:07:04 +0200

    Updated the build system

--- rollup.config.js
+++
@@ -1,7 +0,0 @@
-import resolve from 'rollup-plugin-node-resolve';
-
-export default {
-  input: 'lib/index.mjs',
-  output: {file: 'build/enum.js', format: 'iife', name: 'window', extend: true},
-  plugins: [resolve()]
-};
---
+++ src/rollup.config.js
@@ -0,0 +1,8 @@
+import {join} from 'path';
+import resolve from 'rollup-plugin-node-resolve';
+
+export default {
+  input: join(__dirname, '../lib/index.mjs'),
+  output: {file: join(__dirname, '../build/enum.js'), format: 'iife', name: 'window', extend: true},
+  plugins: [resolve()]
+};
