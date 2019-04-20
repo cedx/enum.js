89ff2745aaed6e2715bd4b6be3cb1e501a8f7a77
Cédric Belin <cedric@belin.io>
2019-04-20 17:09:41 +0200

    Code optimization

--- etc/karma.js
+++ etc/karma.js
@@ -1,17 +1,18 @@
 const {join} = require('path');
-const sources = [join(__dirname, '../src/**/*.ts'), join(__dirname, '../test/**/*_test.ts')];
+const sources = ['src/**/*.ts', 'test/**/*_test.ts'];
 
 module.exports = config => config.set({
+  basePath: join(__dirname, '..'),
   browsers: ['FirefoxHeadless'],
   files: sources,
   frameworks: ['mocha', 'karma-typescript'],
   karmaTypescriptConfig: {
     coverageOptions: {exclude: /_test\.ts$/i},
     include: sources,
-    reports: {lcovonly: {directory: join(__dirname, '..'), filename: 'lcov.info', subdirectory: 'var'}},
-    tsconfig: '../tsconfig.json'
+    reports: {lcovonly: {directory: '..', filename: 'lcov.info', subdirectory: 'var'}},
+    tsconfig: 'tsconfig.json'
   },
-  preprocessors: {[join(__dirname, '../**/*.ts')]: ['karma-typescript']},
+  preprocessors: {'**/*.ts': ['karma-typescript']},
   reporters: ['progress', 'karma-typescript'],
   singleRun: true
 });
