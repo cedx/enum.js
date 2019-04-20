36053ffde175a8f363eb4f2f6ec60f55b871e734
Cédric Belin <cedric@belin.io>
2019-04-20 17:14:07 +0200

    Reverted the previous commit

--- etc/karma.js
+++ etc/karma.js
@@ -1,18 +1,17 @@
 const {join} = require('path');
-const sources = ['src/**/*.ts', 'test/**/*_test.ts'];
+const sources = [join(__dirname, '../src/**/*.ts'), join(__dirname, '../test/**/*_test.ts')];
 
 module.exports = config => config.set({
-  basePath: join(__dirname, '..'),
   browsers: ['FirefoxHeadless'],
   files: sources,
   frameworks: ['mocha', 'karma-typescript'],
   karmaTypescriptConfig: {
     coverageOptions: {exclude: /_test\.ts$/i},
     include: sources,
-    reports: {lcovonly: {directory: '..', filename: 'lcov.info', subdirectory: 'var'}},
-    tsconfig: 'tsconfig.json'
+    reports: {lcovonly: {directory: join(__dirname, '..'), filename: 'lcov.info', subdirectory: 'var'}},
+    tsconfig: '../tsconfig.json'
   },
-  preprocessors: {'**/*.ts': ['karma-typescript']},
+  preprocessors: {[join(__dirname, '../**/*.ts')]: ['karma-typescript']},
   reporters: ['progress', 'karma-typescript'],
   singleRun: true
 });
