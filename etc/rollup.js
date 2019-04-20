856420c3f635e384a96860f8b93e57982b465d5b
Cédric Belin <cedric@belin.io>
2019-04-20 13:09:37 +0200

    Fixed regressions

--- etc/rollup.js
+++ etc/rollup.js
@@ -1,7 +1,8 @@
+import {join} from 'path';
 import resolve from 'rollup-plugin-node-resolve';
 
 export default {
-  input: '../lib/index.mjs',
-  output: {file: '../build/enum.js', format: 'iife', name: 'window', extend: true},
+  input: join(__dirname, '../lib/index.mjs'),
+  output: {file: join(__dirname, '../build/enum.js'), format: 'iife', name: 'window', extend: true},
   plugins: [resolve()]
 };
