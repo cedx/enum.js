d0a007d6af7576d5142568a9e25ff515ab0a3a98
Cédric Belin <cedric@belin.io>
2019-06-10 23:46:11 +0200

    Ported some YAML config files to JSON

--- etc/rollup.js
+++ etc/rollup.js
@@ -2,7 +2,7 @@ import {join} from 'path';
 import resolve from 'rollup-plugin-node-resolve';
 
 export default {
-  input: join(__dirname, '../lib/index.mjs'),
+  input: join(__dirname, '../lib/index.js'),
   output: {file: join(__dirname, '../build/enum.js'), format: 'iife', name: 'window', extend: true},
   plugins: [resolve()]
 };
