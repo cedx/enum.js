dca34a7d44330e3e23c7831cc03c14779ffc0174
Cédric Belin <cedric@belin.io>
2019-06-10 23:45:19 +0200

    Updated the Karma settings

--- etc/karma.js
+++ etc/karma.js
@@ -1,17 +1,10 @@
 const {join} = require('path');
-const sources = [join(__dirname, '../src/**/*.ts'), join(__dirname, '../test/**/*_test.ts')];
+const sources = [join(__dirname, '../lib/**/*.js'), join(__dirname, '../test/**/*_test.ts')];
 
 module.exports = config => config.set({
   browsers: ['FirefoxHeadless'],
   files: sources,
-  frameworks: ['mocha', 'karma-typescript'],
-  karmaTypescriptConfig: {
-    coverageOptions: {exclude: /_test\.ts$/i},
-    include: sources,
-    reports: {lcovonly: {directory: join(__dirname, '..'), filename: 'lcov.info', subdirectory: 'var'}},
-    tsconfig: '../tsconfig.json'
-  },
-  preprocessors: {[join(__dirname, '../**/*.ts')]: ['karma-typescript']},
-  reporters: ['progress', 'karma-typescript'],
+  frameworks: ['mocha'],
+  reporters: ['progress'],
   singleRun: true
 });
