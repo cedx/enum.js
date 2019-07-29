a028edee87eca46da6f84a32bc3c90ce230f2557
Cédric Belin <cedric@belin.io>
2019-07-29 22:52:04 +0200

    Updated the build system

--- gulpfile.esm.js
+++ gulpfile.esm.js
@@ -1,21 +1,18 @@
-import {spawn} from 'child_process';
+import {spawn, SpawnOptions} from 'child_process';
 import del from 'del';
 import {promises} from 'fs';
 import gulp from 'gulp';
 import {delimiter, normalize, resolve} from 'path';
 
-/**
- * The file patterns providing the list of source files.
- * @type {string[]}
- */
-const sources = ['*.js', 'example/*.js', 'lib/**/*.js', 'test/**/*.js'];
+/** The file patterns providing the list of source files. */
+const sources: string[] = ['*.ts', 'example/*.ts', 'src/**/*.ts', 'test/**/*.ts'];
 
 // Shortcuts.
 const {parallel, task, watch} = gulp;
 const {access, copyFile} = promises;
 
 // Initialize the build system.
-const _path = 'PATH' in process.env ? process.env.PATH : '';
+const _path = 'PATH' in process.env ? process.env.PATH! : '';
 const _vendor = resolve('node_modules/.bin');
 if (!_path.includes(_vendor)) process.env.PATH = `${_vendor}${delimiter}${_path}`;
 
@@ -26,7 +23,7 @@ task('build', async () => {
 });
 
 /** Deletes all generated files and reset any saved state. */
-task('clean', () => del(['.nyc_output', 'build', 'doc/api', 'var/**/*', 'web']));
+task('clean', () => del(['.nyc_output', 'build', 'doc/api', 'lib', 'var/**/*', 'web']));
 
 /** Uploads the results of the code coverage. */
 task('coverage', async () => {
@@ -43,7 +40,7 @@ task('coverage', async () => {
 /** Builds the documentation. */
 task('doc', async () => {
   for (const path of ['CHANGELOG.md', 'LICENSE.md']) await copyFile(path, `doc/about/${path.toLowerCase()}`);
-  await _exec('jsdoc', ['--configure', 'etc/jsdoc.json']);
+  await _exec('typedoc', ['--gaID', process.env.GOOGLE_ANALYTICS_ID!, '--options', 'etc/typedoc.json', '--tsconfig', 'src/tsconfig.json']);
   await _exec('mkdocs', ['build', '--config-file=etc/mkdocs.yaml']);
   return del(['doc/about/changelog.md', 'doc/about/license.md']);
 });
@@ -56,7 +53,7 @@ task('lint', () => _exec('eslint', ['--config=etc/eslint.json', ...sources]));
 
 /** Runs the test suites. */
 task('test:browser', () => _exec('karma', ['start', 'etc/karma.js']));
-task('test:node', () => _exec('nyc', ['--nycrc-path=etc/nyc.json', 'node_modules/.bin/mocha', '--config=etc/mocha.json']));
+task('test:node', () => _exec('nyc', ['--nycrc-path=etc/nyc.json', 'node_modules/.bin/mocha', '--config=etc/mocha.json', '"test/**/*.ts"']));
 task('test', parallel('test:browser', 'test:node'));
 
 /** Upgrades the project to the latest revision. */
@@ -70,8 +67,8 @@ task('upgrade', async () => {
 
 /** Watches for file changes. */
 task('watch', () => {
-  watch('lib/**/*.js', {ignoreInitial: false}, task('build'));
-  watch('test/**/*.js', task('test'));
+  watch('src/**/*.ts', {ignoreInitial: false}, task('build'));
+  watch('test/**/*.ts', task('test'));
 });
 
 /** Runs the default tasks. */
@@ -79,28 +76,13 @@ task('default', task('build'));
 
 /**
  * Spawns a new process using the specified command.
- * @param {string} command The command to run.
- * @param {string[]} [args] The command arguments.
- * @param {SpawnOptions} [options] The settings to customize how the process is spawned.
- * @return {Promise} Completes when the command is finally terminated.
+ * @param command The command to run.
+ * @param args The command arguments.
+ * @param options The settings to customize how the process is spawned.
+ * @return Completes when the command is finally terminated.
  */
-function _exec(command, args = [], options = {}) {
+function _exec(command: string, args: string[] = [], options: SpawnOptions = {}): Promise<void> {
   return new Promise((fulfill, reject) => spawn(normalize(command), args, {shell: true, stdio: 'inherit', ...options})
     .on('close', code => code ? reject(new Error(`${command}: ${code}`)) : fulfill())
   );
 }
-
-/**
- * @typedef {object} SpawnOptions
- * @property {string} [argv0] Explicitly set the value of `argv[0]` sent to the child process.
- * @property {string} [cwd] Current working directory of the child process.
- * @property {boolean} [detached] Prepare child to run independently of its parent process.
- * @property {Object<string, string>} [env] Environment key-value pairs.
- * @property {number} [gid] Sets the group identity of the process.
- * @property {boolean|string} [shell] If `true`, runs command inside of a shell. A different shell can be specified as a string.
- * @property {Array|string} [stdio] Child's stdio configuration.
- * @property {number} [timeout] In milliseconds the maximum amount of time the process is allowed to run.
- * @property {number} [uid] Sets the user identity of the process.
- * @property {boolean} [windowsHide] Hide the subprocess console window that would normally be created on Windows systems.
- * @property {boolean} [windowsVerbatimArguments] No quoting or escaping of arguments is done on Windows.
- */
