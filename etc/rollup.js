49aa458a45a97e4c88f66a85d1405be3b7ef30b7
Cédric Belin <cedric@belin.io>
2019-09-28 11:56:14 +0200

    Removed the usage of the `__dirname` value

--- etc/rollup.js
+++ etc/rollup.js
@@ -1,10 +1,8 @@
-import {resolve} from 'path';
-
 export default {
-  input: resolve(__dirname, '../lib/index.js'),
+  input: 'lib/index.js',
   output: {
     extend: true,
-    file: resolve(__dirname, '../build/enum.js'),
+    file: 'build/enum.js',
     format: 'iife',
     name: 'window'
   }
