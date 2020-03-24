9cd11c2b034e857c06bd0a8a9bbf2a8790e42ebe
Cédric Belin <cedric@belin.io>
2020-03-24 18:30:44 +0100

    Replaced `chai` by Node `assert` for testing

--- test/enum_test.js
+++ test/enum_test.js
@@ -1,4 +1,4 @@
-import chai from 'chai';
+import {strict as assert} from 'assert';
 import {Enum} from '../lib/index.js';
 
 /** A JavaScript enumeration providing a mixed set of scalar values. */
@@ -26,186 +26,184 @@ const StringEnum = {
 
 /** Tests the features of the {@link Enum} class. */
 describe('Enum', () => {
-  const {expect} = chai;
-
   describe('.assert()', () => {
     it('should return the specified value if it is a known one', () => {
-      expect(SampleEnum.assert(false)).to.equal(SampleEnum.zero);
-      expect(SampleEnum.assert(1)).to.equal(SampleEnum.one);
-      expect(SampleEnum.assert('TWO')).to.equal(SampleEnum.two);
-      expect(SampleEnum.assert(3.0)).to.equal(SampleEnum.three);
+      assert.equal(SampleEnum.assert(false), SampleEnum.zero);
+      assert.equal(SampleEnum.assert(1), SampleEnum.one);
+      assert.equal(SampleEnum.assert('TWO'), SampleEnum.two);
+      assert.equal(SampleEnum.assert(3.0), SampleEnum.three);
 
-      expect(Enum.assert(NumericEnum, 1)).to.equal(NumericEnum.one);
-      expect(Enum.assert(StringEnum, 'ONE')).to.equal(StringEnum.one);
+      assert.equal(Enum.assert(NumericEnum, 1), NumericEnum.one);
+      assert.equal(Enum.assert(StringEnum, 'ONE'), StringEnum.one);
     });
 
     it('should throw an exception if it is an unknown value', () => {
-      expect(() => SampleEnum.assert('')).to.throw(TypeError);
-      expect(() => SampleEnum.assert('two')).to.throw(TypeError);
-      expect(() => SampleEnum.assert(3.1)).to.throw(TypeError);
+      assert.throws(() => SampleEnum.assert(''), TypeError);
+      assert.throws(() => SampleEnum.assert('two'), TypeError);
+      assert.throws(() => SampleEnum.assert(3.1), TypeError);
 
-      expect(() => Enum.assert(NumericEnum, 0)).to.throw(TypeError);
-      expect(() => Enum.assert(StringEnum, 'zero')).to.throw(TypeError);
+      assert.throws(() => Enum.assert(NumericEnum, 0), TypeError);
+      assert.throws(() => Enum.assert(StringEnum, 'zero'), TypeError);
 
       // Edge case: reverse mapping of numeric enums.
-      expect(() => Enum.assert(NumericEnum, 'one')).to.throw(TypeError);
+      assert.throws(() => Enum.assert(NumericEnum, 'one'), TypeError);
     });
   });
 
   describe('.coerce()', () => {
     it('should return the specified value if it is a known one', () => {
-      expect(SampleEnum.coerce(false)).to.equal(SampleEnum.zero);
-      expect(SampleEnum.coerce(1)).to.equal(SampleEnum.one);
-      expect(SampleEnum.coerce('TWO')).to.equal(SampleEnum.two);
-      expect(SampleEnum.coerce(3.0)).to.equal(SampleEnum.three);
+      assert.equal(SampleEnum.coerce(false), SampleEnum.zero);
+      assert.equal(SampleEnum.coerce(1), SampleEnum.one);
+      assert.equal(SampleEnum.coerce('TWO'), SampleEnum.two);
+      assert.equal(SampleEnum.coerce(3.0), SampleEnum.three);
 
-      expect(Enum.coerce(NumericEnum, 1)).to.equal(NumericEnum.one);
-      expect(Enum.coerce(StringEnum, 'ONE')).to.equal(StringEnum.one);
+      assert.equal(Enum.coerce(NumericEnum, 1), NumericEnum.one);
+      assert.equal(Enum.coerce(StringEnum, 'ONE'), StringEnum.one);
     });
 
     it('should return the default value if it is an unknown one', () => {
-      expect(SampleEnum.coerce('')).to.be.undefined;
-      expect(SampleEnum.coerce('two', false)).to.be.false;
-      expect(SampleEnum.coerce(3.1, SampleEnum.three)).to.equal(SampleEnum.three);
+      assert.equal(SampleEnum.coerce(''), undefined);
+      assert.equal(SampleEnum.coerce('two', false), false);
+      assert.equal(SampleEnum.coerce(3.1, SampleEnum.three), SampleEnum.three);
 
-      expect(Enum.coerce(NumericEnum, 0)).to.be.undefined;
-      expect(Enum.coerce(NumericEnum, 0, NumericEnum.eight)).to.equal(8);
+      assert.equal(Enum.coerce(NumericEnum, 0), undefined);
+      assert.equal(Enum.coerce(NumericEnum, 0, NumericEnum.eight), 8);
 
-      expect(Enum.coerce(StringEnum, 'zero')).to.be.undefined;
-      expect(Enum.coerce(StringEnum, 'zero', StringEnum.three)).to.equal('THREE');
+      assert.equal(Enum.coerce(StringEnum, 'zero'), undefined);
+      assert.equal(Enum.coerce(StringEnum, 'zero', StringEnum.three), 'THREE');
 
       // Edge case: reverse mapping of numeric enums.
-      expect(Enum.coerce(NumericEnum, 'one')).to.be.undefined;
+      assert.equal(Enum.coerce(NumericEnum, 'one'), undefined);
     });
   });
 
   describe('.create()', () => {
     it('should create types that are immutable', () => {
-      expect(SampleEnum).to.be.frozen;
+      assert(Object.isFrozen(SampleEnum));
     });
 
     it('should create types having the `Enum` mixin', () => {
       const methods = ['assert', 'coerce', 'entries', 'getIndex', 'getName', 'isDefined', 'names', 'values'];
-      for (const method of methods) expect(SampleEnum[method]).to.be.a('function');
+      assert(methods.every(method => typeof SampleEnum[method] == 'function'));
     });
   });
 
   describe('.entries()', () => {
     it('should return the pairs of names and values of the enumerated constants', () => {
       let entries = SampleEnum.entries();
-      expect(entries).to.have.lengthOf(4);
+      assert.equal(entries.length, 4);
 
       let [tuple1, tuple2, tuple3, tuple4] = entries;
-      expect(tuple1).to.have.ordered.members(['zero', false]);
-      expect(tuple2).to.have.ordered.members(['one', 1]);
-      expect(tuple3).to.have.ordered.members(['two', 'TWO']);
-      expect(tuple4).to.have.ordered.members(['three', 3.0]);
+      assert.deepEqual(tuple1, ['zero', false]);
+      assert.deepEqual(tuple2, ['one', 1]);
+      assert.deepEqual(tuple3, ['two', 'TWO']);
+      assert.deepEqual(tuple4, ['three', 3.0]);
 
       entries = Enum.entries(NumericEnum);
-      expect(entries).to.have.lengthOf(4);
+      assert.equal(entries.length, 4);
 
       [tuple1, tuple2, tuple3, tuple4] = entries;
-      expect(tuple1).to.have.ordered.members(['one', 1]);
-      expect(tuple2).to.have.ordered.members(['two', 2]);
-      expect(tuple3).to.have.ordered.members(['four', 4]);
-      expect(tuple4).to.have.ordered.members(['eight', 8]);
+      assert.deepEqual(tuple1, ['one', 1]);
+      assert.deepEqual(tuple2, ['two', 2]);
+      assert.deepEqual(tuple3, ['four', 4]);
+      assert.deepEqual(tuple4, ['eight', 8]);
 
       entries = Enum.entries(StringEnum);
-      expect(entries).to.have.lengthOf(4);
+      assert.equal(entries.length, 4);
 
       [tuple1, tuple2, tuple3, tuple4] = entries;
-      expect(tuple1).to.have.ordered.members(['zero', 'ZERO']);
-      expect(tuple2).to.have.ordered.members(['one', 'ONE']);
-      expect(tuple3).to.have.ordered.members(['two', 'TWO']);
-      expect(tuple4).to.have.ordered.members(['three', 'THREE']);
+      assert.deepEqual(tuple1, ['zero', 'ZERO']);
+      assert.deepEqual(tuple2, ['one', 'ONE']);
+      assert.deepEqual(tuple3, ['two', 'TWO']);
+      assert.deepEqual(tuple4, ['three', 'THREE']);
     });
   });
 
   describe('.getIndex()', () => {
     it('should return `-1` for unknown values', () => {
-      expect(SampleEnum.getIndex(0)).to.equal(-1);
-      expect(SampleEnum.getIndex('two')).to.equal(-1);
-      expect(SampleEnum.getIndex(3.1)).to.equal(-1);
+      assert.equal(SampleEnum.getIndex(0), -1);
+      assert.equal(SampleEnum.getIndex('two'), -1);
+      assert.equal(SampleEnum.getIndex(3.1), -1);
 
-      expect(Enum.getIndex(NumericEnum, 0)).to.equal(-1);
-      expect(Enum.getIndex(StringEnum, 'zero')).to.equal(-1);
+      assert.equal(Enum.getIndex(NumericEnum, 0), -1);
+      assert.equal(Enum.getIndex(StringEnum, 'zero'), -1);
 
       // Edge case: reverse mapping of numeric enums.
-      expect(Enum.getIndex(NumericEnum, 'one')).to.equal(-1);
+      assert.equal(Enum.getIndex(NumericEnum, 'one'), -1);
     });
 
     it('should return the index of the enumerated constant for known values', () => {
-      expect(SampleEnum.getIndex(false)).to.equal(0);
-      expect(SampleEnum.getIndex(1)).to.equal(1);
-      expect(SampleEnum.getIndex('TWO')).to.equal(2);
-      expect(SampleEnum.getIndex(3.0)).to.equal(3);
+      assert.equal(SampleEnum.getIndex(false), 0);
+      assert.equal(SampleEnum.getIndex(1), 1);
+      assert.equal(SampleEnum.getIndex('TWO'), 2);
+      assert.equal(SampleEnum.getIndex(3.0), 3);
 
-      expect(Enum.getIndex(NumericEnum, 1)).to.equal(0);
-      expect(Enum.getIndex(StringEnum, 'ONE')).to.equal(1);
+      assert.equal(Enum.getIndex(NumericEnum, 1), 0);
+      assert.equal(Enum.getIndex(StringEnum, 'ONE'), 1);
     });
   });
 
   describe('.getName()', () => {
     it('should return an empty string for unknown values', () => {
-      expect(SampleEnum.getName(0)).to.be.empty;
-      expect(SampleEnum.getName('two')).to.be.empty;
-      expect(SampleEnum.getName(3.1)).to.be.empty;
+      assert.equal(SampleEnum.getName(0).length, 0);
+      assert.equal(SampleEnum.getName('two').length, 0);
+      assert.equal(SampleEnum.getName(3.1).length, 0);
 
-      expect(Enum.getName(NumericEnum, 0)).to.be.empty;
-      expect(Enum.getName(StringEnum, 'zero')).to.be.empty;
+      assert.equal(Enum.getName(NumericEnum, 0).length, 0);
+      assert.equal(Enum.getName(StringEnum, 'zero').length, 0);
 
       // Edge case: reverse mapping of numeric enums.
-      expect(Enum.getName(NumericEnum, 'one')).to.be.empty;
+      assert.equal(Enum.getName(NumericEnum, 'one').length, 0);
     });
 
     it('should return the name for known values', () => {
-      expect(SampleEnum.getName(false)).to.equal('zero');
-      expect(SampleEnum.getName(1)).to.equal('one');
-      expect(SampleEnum.getName('TWO')).to.equal('two');
-      expect(SampleEnum.getName(3.0)).to.equal('three');
+      assert.equal(SampleEnum.getName(false), 'zero');
+      assert.equal(SampleEnum.getName(1), 'one');
+      assert.equal(SampleEnum.getName('TWO'), 'two');
+      assert.equal(SampleEnum.getName(3.0), 'three');
 
-      expect(Enum.getName(NumericEnum, 1)).to.equal('one');
-      expect(Enum.getName(StringEnum, 'ONE')).to.equal('one');
+      assert.equal(Enum.getName(NumericEnum, 1), 'one');
+      assert.equal(Enum.getName(StringEnum, 'ONE'), 'one');
     });
   });
 
   describe('.isDefined()', () => {
     it('should return `false` for unknown values', () => {
-      expect(SampleEnum.isDefined(0)).to.be.false;
-      expect(SampleEnum.isDefined('two')).to.be.false;
-      expect(SampleEnum.isDefined(3.1)).to.be.false;
+      assert.equal(SampleEnum.isDefined(0), false);
+      assert.equal(SampleEnum.isDefined('two'), false);
+      assert.equal(SampleEnum.isDefined(3.1), false);
 
-      expect(Enum.isDefined(NumericEnum, 0)).to.be.false;
-      expect(Enum.isDefined(StringEnum, 'zero')).to.be.false;
+      assert.equal(Enum.isDefined(NumericEnum, 0), false);
+      assert.equal(Enum.isDefined(StringEnum, 'zero'), false);
 
       // Edge case: reverse mapping of numeric enums.
-      expect(Enum.isDefined(NumericEnum, 'one')).to.be.false;
+      assert.equal(Enum.isDefined(NumericEnum, 'one'), false);
     });
 
     it('should return `true` for known values', () => {
-      expect(SampleEnum.isDefined(false)).to.be.true;
-      expect(SampleEnum.isDefined(1)).to.be.true;
-      expect(SampleEnum.isDefined('TWO')).to.be.true;
-      expect(SampleEnum.isDefined(3.0)).to.be.true;
+      assert(SampleEnum.isDefined(false));
+      assert(SampleEnum.isDefined(1));
+      assert(SampleEnum.isDefined('TWO'));
+      assert(SampleEnum.isDefined(3.0));
 
-      expect(Enum.isDefined(NumericEnum, 1)).to.be.true;
-      expect(Enum.isDefined(StringEnum, 'ONE')).to.be.true;
+      assert(Enum.isDefined(NumericEnum, 1));
+      assert(Enum.isDefined(StringEnum, 'ONE'));
     });
   });
 
   describe('.names()', () => {
     it('should return the names of the enumerable properties', () => {
-      expect(SampleEnum.names()).to.have.ordered.members(['zero', 'one', 'two', 'three']);
-      expect(Enum.names(NumericEnum)).to.have.ordered.members(['one', 'two', 'four', 'eight']);
-      expect(Enum.names(StringEnum)).to.have.ordered.members(['zero', 'one', 'two', 'three']);
+      assert.deepEqual(SampleEnum.names(), ['zero', 'one', 'two', 'three']);
+      assert.deepEqual(Enum.names(NumericEnum), ['one', 'two', 'four', 'eight']);
+      assert.deepEqual(Enum.names(StringEnum), ['zero', 'one', 'two', 'three']);
     });
   });
 
   describe('.values()', () => {
     it('should return the values of the enumerable properties', () => {
-      expect(SampleEnum.values()).to.have.ordered.members([false, 1, 'TWO', 3.0]);
-      expect(Enum.values(NumericEnum)).to.have.ordered.members([1, 2, 4, 8]);
-      expect(Enum.values(StringEnum)).to.have.ordered.members(['ZERO', 'ONE', 'TWO', 'THREE']);
+      assert.deepEqual(SampleEnum.values(), [false, 1, 'TWO', 3.0]);
+      assert.deepEqual(Enum.values(NumericEnum), [1, 2, 4, 8]);
+      assert.deepEqual(Enum.values(StringEnum), ['ZERO', 'ONE', 'TWO', 'THREE']);
     });
   });
 });
