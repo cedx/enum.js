143d8473552164ffb8a4571d18591bf622fd5c9a
Cédric Belin <cedric@belin.io>
2020-05-11 00:39:32 +0200

    Fixed the build system

--- tool/build.ps1
+++ tool/build.ps1
@@ -1,4 +1,4 @@
 #!/usr/bin/env pwsh
 Set-StrictMode -Version Latest
 Set-Location (Split-Path $PSScriptRoot)
-node_modules/.bin/tsc.ps1 --project src/tsconfig.json
+node_modules/.bin/tsc --project src/tsconfig.json
