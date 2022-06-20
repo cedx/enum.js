e5916dafb95c4c5335ddcf1cefcff081ea8874df
Cédric Belin <cedric@belin.io>
2022-06-20 22:35:16 +0200

    Fixed the warnings of the static analyzer

--- test/enum_test.js
+++ test/enum_test.js
@@ -1,4 +1,3 @@
-/* eslint-disable max-lines-per-function */
 import assert from "node:assert/strict";
 import test from "node:test";
 import {Enum} from "../lib/enum.js";
@@ -14,38 +13,38 @@ const SampleEnum = Object.freeze({
 	three: 3.0
 });
 
-test("Enum.assert()", async t => {
-	await t.test("should return the specified value if it is a known one", () => {
+test("Enum.assert()", async ctx => {
+	await ctx.test("should return the specified value if it is a known one", () => {
 		assert.equal(Enum.assert(SampleEnum, false), SampleEnum.zero);
 		assert.equal(Enum.assert(SampleEnum, 1), SampleEnum.one);
 		assert.equal(Enum.assert(SampleEnum, "TWO"), SampleEnum.two);
 		assert.equal(Enum.assert(SampleEnum, 3.0), SampleEnum.three);
 	});
 
-	await t.test("should throw an exception if it is an unknown value", () => {
+	await ctx.test("should throw an exception if it is an unknown value", () => {
 		assert.throws(() => Enum.assert(SampleEnum, ""), TypeError);
 		assert.throws(() => Enum.assert(SampleEnum, "two"), TypeError);
 		assert.throws(() => Enum.assert(SampleEnum, 3.1), TypeError);
 	});
 });
 
-test("Enum.coerce()", async t => {
-	await t.test("should return the specified value if it is a known one", () => {
+test("Enum.coerce()", async ctx => {
+	await ctx.test("should return the specified value if it is a known one", () => {
 		assert.equal(Enum.coerce(SampleEnum, false, SampleEnum.zero), SampleEnum.zero);
 		assert.equal(Enum.coerce(SampleEnum, 1, SampleEnum.zero), SampleEnum.one);
 		assert.equal(Enum.coerce(SampleEnum, "TWO", SampleEnum.zero), SampleEnum.two);
 		assert.equal(Enum.coerce(SampleEnum, 3.0, SampleEnum.zero), SampleEnum.three);
 	});
 
-	await t.test("should return the default value if it is an unknown one", () => {
+	await ctx.test("should return the default value if it is an unknown one", () => {
 		assert.equal(Enum.coerce(SampleEnum, "", SampleEnum.zero), SampleEnum.zero);
 		assert.equal(Enum.coerce(SampleEnum, "two", SampleEnum.two), SampleEnum.two);
 		assert.equal(Enum.coerce(SampleEnum, 3.1, SampleEnum.two), SampleEnum.two);
 	});
 });
 
-test("Enum.getEntries()", async t => {
-	await t.test("should return the pairs of names and values", () => {
+test("Enum.getEntries()", async ctx => {
+	await ctx.test("should return the pairs of names and values", () => {
 		const entries = Enum.getEntries(SampleEnum);
 		assert.equal(entries.size, 4);
 
@@ -57,14 +56,14 @@ test("Enum.getEntries()", async t => {
 	});
 });
 
-test("Enum.getIndex()", async t => {
-	await t.test("should return `-1` for unknown values", () => {
+test("Enum.getIndex()", async ctx => {
+	await ctx.test("should return `-1` for unknown values", () => {
 		assert.equal(Enum.getIndex(SampleEnum, 0), -1);
 		assert.equal(Enum.getIndex(SampleEnum, "two"), -1);
 		assert.equal(Enum.getIndex(SampleEnum, 3.1), -1);
 	});
 
-	await t.test("should return the index for known values", () => {
+	await ctx.test("should return the index for known values", () => {
 		assert.equal(Enum.getIndex(SampleEnum, false), 0);
 		assert.equal(Enum.getIndex(SampleEnum, 1), 1);
 		assert.equal(Enum.getIndex(SampleEnum, "TWO"), 2);
@@ -72,14 +71,14 @@ test("Enum.getIndex()", async t => {
 	});
 });
 
-test("Enum.getName()", async t => {
-	await t.test("should return an empty string for unknown values", () => {
+test("Enum.getName()", async ctx => {
+	await ctx.test("should return an empty string for unknown values", () => {
 		assert.equal(Enum.getName(SampleEnum, 0).length, 0);
 		assert.equal(Enum.getName(SampleEnum, "two").length, 0);
 		assert.equal(Enum.getName(SampleEnum, 3.1).length, 0);
 	});
 
-	await t.test("should return the name for known values", () => {
+	await ctx.test("should return the name for known values", () => {
 		assert.equal(Enum.getName(SampleEnum, false), "zero");
 		assert.equal(Enum.getName(SampleEnum, 1), "one");
 		assert.equal(Enum.getName(SampleEnum, "TWO"), "two");
@@ -87,26 +86,26 @@ test("Enum.getName()", async t => {
 	});
 });
 
-test("Enum.getNames()", async t => {
-	await t.test("should return the names of the enumerable properties", () => {
+test("Enum.getNames()", async ctx => {
+	await ctx.test("should return the names of the enumerable properties", () => {
 		assert.deepEqual(Enum.getNames(SampleEnum), ["zero", "one", "two", "three"]);
 	});
 });
 
-test("Enum.getValues()", async t => {
-	await t.test("should return the values of the enumerable properties", () => {
+test("Enum.getValues()", async ctx => {
+	await ctx.test("should return the values of the enumerable properties", () => {
 		assert.deepEqual(Enum.getValues(SampleEnum), [false, 1, "TWO", 3.0]);
 	});
 });
 
-test("Enum.isDefined()", async t => {
-	await t.test("should return `false` for unknown values", () => {
+test("Enum.isDefined()", async ctx => {
+	await ctx.test("should return `false` for unknown values", () => {
 		assert.ok(!Enum.isDefined(SampleEnum, 0));
 		assert.ok(!Enum.isDefined(SampleEnum, "two"));
 		assert.ok(!Enum.isDefined(SampleEnum, 3.1));
 	});
 
-	await t.test("should return `true` for known values", () => {
+	await ctx.test("should return `true` for known values", () => {
 		assert.ok(Enum.isDefined(SampleEnum, false));
 		assert.ok(Enum.isDefined(SampleEnum, 1));
 		assert.ok(Enum.isDefined(SampleEnum, "TWO"));
--- test/index_test.js
+++ test/index_test.js
@@ -1,4 +1,3 @@
-/* eslint-disable max-lines-per-function */
 import assert from "node:assert/strict";
 import test from "node:test";
 import {Enum} from "../lib/enum.js";
@@ -15,49 +14,49 @@ const SampleEnum = createEnum({
 	three: 3.0
 });
 
-test("createEnum()", async t => {
-	await t.test("should create types that are immutable", () => {
+test("createEnum()", async ctx => {
+	await ctx.test("should create types that are immutable", () => {
 		assert.ok(Object.isFrozen(SampleEnum));
 	});
 
-	await t.test("should create types having the `EnumMixin` mixin", () => {
+	await ctx.test("should create types having the `EnumMixin` mixin", () => {
 		const methods = Reflect.ownKeys(Enum).filter(key => typeof Reflect.get(Enum, key) == "function");
 		assert.ok(methods.every(method => typeof Reflect.get(SampleEnum, method) == "function"));
 	});
 });
 
-test("createEnum().assert()", async t => {
-	await t.test("should return the specified value if it is a known one", () => {
+test("createEnum().assert()", async ctx => {
+	await ctx.test("should return the specified value if it is a known one", () => {
 		assert.equal(SampleEnum.assert(false), SampleEnum.zero);
 		assert.equal(SampleEnum.assert(1), SampleEnum.one);
 		assert.equal(SampleEnum.assert("TWO"), SampleEnum.two);
 		assert.equal(SampleEnum.assert(3.0), SampleEnum.three);
 	});
 
-	await t.test("should throw an exception if it is an unknown value", () => {
+	await ctx.test("should throw an exception if it is an unknown value", () => {
 		assert.throws(() => SampleEnum.assert(""), TypeError);
 		assert.throws(() => SampleEnum.assert("two"), TypeError);
 		assert.throws(() => SampleEnum.assert(3.1), TypeError);
 	});
 });
 
-test("createEnum().coerce()", async t => {
-	await t.test("should return the specified value if it is a known one", () => {
+test("createEnum().coerce()", async ctx => {
+	await ctx.test("should return the specified value if it is a known one", () => {
 		assert.equal(SampleEnum.coerce(false, SampleEnum.zero), SampleEnum.zero);
 		assert.equal(SampleEnum.coerce(1, SampleEnum.zero), SampleEnum.one);
 		assert.equal(SampleEnum.coerce("TWO", SampleEnum.zero), SampleEnum.two);
 		assert.equal(SampleEnum.coerce(3.0, SampleEnum.zero), SampleEnum.three);
 	});
 
-	await t.test("should return the default value if it is an unknown one", () => {
+	await ctx.test("should return the default value if it is an unknown one", () => {
 		assert.equal(SampleEnum.coerce("", SampleEnum.zero), SampleEnum.zero);
 		assert.equal(SampleEnum.coerce("two", SampleEnum.two), SampleEnum.two);
 		assert.equal(SampleEnum.coerce(3.1, SampleEnum.two), SampleEnum.two);
 	});
 });
 
-test("createEnum().getEntries()", async t => {
-	await t.test("should return the pairs of names and values", () => {
+test("createEnum().getEntries()", async ctx => {
+	await ctx.test("should return the pairs of names and values", () => {
 		const entries = SampleEnum.getEntries();
 		assert.equal(entries.size, 4);
 
@@ -69,14 +68,14 @@ test("createEnum().getEntries()", async t => {
 	});
 });
 
-test("createEnum().getIndex()", async t => {
-	await t.test("should return `-1` for unknown values", () => {
+test("createEnum().getIndex()", async ctx => {
+	await ctx.test("should return `-1` for unknown values", () => {
 		assert.equal(SampleEnum.getIndex(0), -1);
 		assert.equal(SampleEnum.getIndex("two"), -1);
 		assert.equal(SampleEnum.getIndex(3.1), -1);
 	});
 
-	await t.test("should return the index for known values", () => {
+	await ctx.test("should return the index for known values", () => {
 		assert.equal(SampleEnum.getIndex(false), 0);
 		assert.equal(SampleEnum.getIndex(1), 1);
 		assert.equal(SampleEnum.getIndex("TWO"), 2);
@@ -84,14 +83,14 @@ test("createEnum().getIndex()", async t => {
 	});
 });
 
-test("createEnum().getName()", async t => {
-	await t.test("should return an empty string for unknown values", () => {
+test("createEnum().getName()", async ctx => {
+	await ctx.test("should return an empty string for unknown values", () => {
 		assert.equal(SampleEnum.getName(0).length, 0);
 		assert.equal(SampleEnum.getName("two").length, 0);
 		assert.equal(SampleEnum.getName(3.1).length, 0);
 	});
 
-	await t.test("should return the name for known values", () => {
+	await ctx.test("should return the name for known values", () => {
 		assert.equal(SampleEnum.getName(false), "zero");
 		assert.equal(SampleEnum.getName(1), "one");
 		assert.equal(SampleEnum.getName("TWO"), "two");
@@ -99,26 +98,26 @@ test("createEnum().getName()", async t => {
 	});
 });
 
-test("createEnum().getNames()", async t => {
-	await t.test("should return the names of the enumerable properties", () => {
+test("createEnum().getNames()", async ctx => {
+	await ctx.test("should return the names of the enumerable properties", () => {
 		assert.deepEqual(SampleEnum.getNames(), ["zero", "one", "two", "three"]);
 	});
 });
 
-test("createEnum().getValues()", async t => {
-	await t.test("should return the values of the enumerable properties", () => {
+test("createEnum().getValues()", async ctx => {
+	await ctx.test("should return the values of the enumerable properties", () => {
 		assert.deepEqual(SampleEnum.getValues(), [false, 1, "TWO", 3.0]);
 	});
 });
 
-test("createEnum().isDefined()", async t => {
-	await t.test("should return `false` for unknown values", () => {
+test("createEnum().isDefined()", async ctx => {
+	await ctx.test("should return `false` for unknown values", () => {
 		assert.ok(!SampleEnum.isDefined(0));
 		assert.ok(!SampleEnum.isDefined("two"));
 		assert.ok(!SampleEnum.isDefined(3.1));
 	});
 
-	await t.test("should return `true` for known values", () => {
+	await ctx.test("should return `true` for known values", () => {
 		assert.ok(SampleEnum.isDefined(false));
 		assert.ok(SampleEnum.isDefined(1));
 		assert.ok(SampleEnum.isDefined("TWO"));
