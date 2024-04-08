6c8f16377bb805cb29f0c56b115e87e144624d44
Cédric Belin <cedric@belin.io>
2024-04-08 16:40:47 +0200

    Remove the API reference [skip ci]

--- docs/api/assets/navigation.js
+++
@@ -1,1 +0,0 @@
-window.navigationData = "data:application/octet-stream;base64,H4sIAAAAAAAACouuVipJrShRslJyzSvNVdJRKkgsyVCyUsrMK0ktSktMTi3WB0noZZTk5ijpKGVn5qUoWRmZmtXqwDWmpKYlluaUIPSmleYll2Tm5xXrQ6VQdZuZ1MYCALAOnzF3AAAA"
